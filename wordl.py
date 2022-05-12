81f4f14d45cd4c4119c16f9c86f77ef8c54d737f
marinbursac <105432456+marinbursac@users.noreply.github.com>
2022-05-12 15:31:15 +0200

    Add files via upload

--- wordl.py
+++ wordl.py
@@ -7,11 +7,28 @@ def main():
     pygame.display.set_icon(logo)
     pygame.display.set_caption("wordl")
 
-    screen = pygame.display.set_mode((400,550))
+    green = pygame.image.load("green.png")
+    yellow = pygame.image.load("yellow.png")
+    gray = pygame.image.load("gray.png")
+    border = pygame.image.load("border.png")
+
+    screen = pygame.display.set_mode((350,390))
 
     running = True
-
+    
     while running:
+        
+        screen.fill((255,255,255))
+        for i in range(5):
+            for j in range(6):
+                screen.blit(border, (64*i + 15,64*j))
+        
+        pygame.display.flip()
+
+
+
+
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
