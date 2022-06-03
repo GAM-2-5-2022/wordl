0af04637b50122ea934b23e6a5e1004783461d41
marinbursac <105432456+marinbursac@users.noreply.github.com>
2022-06-03 12:19:42 +0200

    Add files via upload

--- wordl.py
+++ wordl.py
@@ -6,9 +6,10 @@ words = f.readlines()
 guess = 0
 user_text = ''
 render_text = ''
-old_guess = ''
+old_guesses = []
 state = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 temp_state = [0,0,0,0,0]
+end = False
 
 def check():
     global user_text
@@ -22,6 +23,7 @@ def check():
 ##        return -1
     elif user_text == answer:
         temp_state[0:5] = 1
+        end = True
         return 1
     else:
         for i in range(5):
@@ -33,7 +35,7 @@ def check():
                     temp_state[i] = -1
             else:
                 temp_state[i] = 2
-        old_guess = user_text
+        old_guesses.append(user_text)
         user_text = ''
         return 0
 
@@ -61,22 +63,30 @@ print(answer)
 font = pygame.font.Font(None,60)
 
 screen = pygame.display.set_mode((350,390))
+width = screen.get_width()
+height = screen.get_height()
 
 running = True
 while running:
 
+    mouse = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_BACKSPACE:
-                user_text = user_text[:-1]
-            elif (len(user_text) < 5) & (event.key != pygame.K_RETURN):
-                user_text += event.unicode
-            elif event.key == pygame.K_RETURN:
-                guess += 1
-                print(check())
-                update_state()
+            if end == False:
+                if event.key == pygame.K_BACKSPACE:
+                    user_text = user_text[:-1]
+                elif (len(user_text) < 5) & (event.key != pygame.K_RETURN):
+                    user_text += event.unicode
+                elif event.key == pygame.K_RETURN:
+                    print(check())
+                    update_state()
+                    guess += 1
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if width/3 <= mouse[0] <= 2*width/3 and 2*height/5 >= mouse[1] >= height/5:
+                if end == True:
+                    print("slay")
             
     
     screen.fill((255,255,255))
@@ -92,13 +102,34 @@ while running:
             else:
                 screen.blit(gray, (64*i + 15,64*j))
 
-
+    if len(old_guesses) > 0:
+        for i in range(len(old_guesses)):
+            render_text = ''
+            for j in range(5):
+                render_text += old_guesses[i][j] + '   '
+            render_text = render_text.upper()
+            old = font.render(render_text, True, (0,0,0))
+            screen.blit(old, (33,64*i+10))
     user_text = user_text.upper()
     render_text = ''
     for i in range(len(user_text)):
         render_text += user_text[i] + '   '
     surface = font.render(render_text, True, (0,0,0))
     screen.blit(surface, (33,64*guess+10))
+
+    if end == True:
+        if width/3 <= mouse[0] <= 2*width/3 and 2*height/5 >= mouse[1] >= height/5:
+            pygame.draw.rect(screen,(170,170,170),[width/3,height/5,width/3,height/5])
+              
+        else:
+            pygame.draw.rect(screen,(100,100,100),[width/3,height/5,width/3,height/5])
+
+        if width/3 <= mouse[0] <= 2*width/3 and 4*height/5 >= mouse[1] >= 3*height/5:
+            pygame.draw.rect(screen,(170,170,170),[width/3,3*height/5,width/3,height/5])
+              
+        else:
+            pygame.draw.rect(screen,(100,100,100),[width/3,3*height/5,width/3,height/5])
+
     
     pygame.display.flip()
 
